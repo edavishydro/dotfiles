ba7212a4d9b6ff7c9b27a800c7683a6143a83e68
Eddie Davis <edward.davis@wpromote.com>
2025-04-24 08:24:29 -0600

    init chezmoi commit

---
+++ dot_wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- config.color_scheme = "Catppuccin Frappe"
+local scheme = wezterm.color.get_builtin_schemes()["Everforest Dark Medium (Gogh)"]
+scheme.foreground = "#dddecf"
+-- scheme.color_03 = "#bcd19c"
+scheme.ansi[3] = "#bcd19c"
+scheme.ansi[8] = "#dddecf"
+scheme.cursor_fg = "#dddecf"
+
+config.color_schemes = {
+	["Everforest Dark Medium"] = scheme,
+}
+
+config.color_scheme = "Everforest Dark Medium"
+
+config.font = wezterm.font("SFMono Nerd Font", { weight = "Regular" })
+-- config.font = wezterm.font("Dank Mono", { weight = "Regular" })
+config.line_height = 1.2
+config.font_size = 16.0
+
+return config
